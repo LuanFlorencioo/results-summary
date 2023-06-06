cfc9d597cc37899878244859fd027b86ba6d8385
Luan Florencio <luanflorencio.dev@gmail.com>
2023-06-06 12:51:21 -0300

    feat(components): create CardResult component

---
+++ src/components/CardResult/index.tsx
@@ -0,0 +1,23 @@
+import CircleStats from "../CircleStats";
+
+const CardResult = () => {
+  return (
+    <header className="w-full max-w-[400px] px-10 py-6 flex flex-col items-center gap-5 bg-gradient-to-b from-gradient-card-100 to-gradient-card-200 rounded-b-3xl">
+      <p className="text-base font-medium text-grey-200">
+        Your Result
+      </p>
+
+      <CircleStats />
+
+      <p className="text-xl font-bold text-white">
+        Great
+      </p>
+
+      <p className="max-w-[250px] text-sm font-medium text-grey-200 text-center">
+        You scored higher than 65% of the people who have taken these tests.
+      </p>
+    </header>
+  )
+}
+
+export default CardResult;
