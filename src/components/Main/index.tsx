017e1eddf54a63b97c29401eae047d1ad65a5951
Luan Florencio <luanflorencio.dev@gmail.com>
2023-06-09 19:59:08 -0300

    fix(font): adjust font default in App

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,9 @@
 import CardResult from "../CardResult";
 import CardSummary from "../CardSummary";
-import font from "@/styles/font";
 
 const Main = () => {
   return (
-    <main className={`${font.className} w-full h-screen bg-white`}>
+    <main className={`w-full h-screen bg-white`}>
       <CardResult />
       <CardSummary />
     </main>
