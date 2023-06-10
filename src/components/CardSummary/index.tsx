53b07f580afd7d87072d3c9541d0f462b3d1a493
Luan Florencio <luanflorencio.dev@gmail.com>
2023-06-09 23:40:28 -0300

    feat(components): add desktop version

--- src/components/CardSummary/index.tsx
+++ src/components/CardSummary/index.tsx
@@ -4,8 +4,8 @@ import Hability from "../Hability";
 const CardSummary = () => {
 
   return (
-    <section className="w-full px-5 py-6 flex flex-col gap-6">
-      <h2 className="text-base font-semibold text-grey-100" >Summary</h2>
+    <section className="w-full max-w-[400px] mx-auto px-5 py-6 flex flex-col gap-6 selection:bg-gradient-card-200 selection:text-gradient-card-100 md:w-[380px] md:h-[380px] md:mx-0 md:pl-12 md:pr-6 md:gap-4 md:translate-x-[-12px] md:shadow-xl">
+      <h2 className="text-base font-semibold text-grey-100">Summary</h2>
 
       {
         data.map(({ color, name, quantity}, i) => (
@@ -13,7 +13,7 @@ const CardSummary = () => {
         ))
       }
 
-      <button className="w-full py-3 bg-grey-100 rounded-full text-base font-semibold text-white">
+      <button className="w-full py-3 bg-grey-100 rounded-full text-base font-semibold text-white hover:transition ease-out duration-500 hover:bg-gradient-to-b hover:from-gradient-card-100 hover:to-gradient-card-200 ">
         Continue
       </button>
     </section>
